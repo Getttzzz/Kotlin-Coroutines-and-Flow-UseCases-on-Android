a641bca7a23bc82ce6cc5851757e88a074289d23
Yurii Hetsko <getttzzz@gmail.com>
2024-06-12 21:54:36 +0200

    retry simple function

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,44 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            val numberOfRetries = 2
+            try {
+                myRetry(numberOfRetries) {
+                    loadRequest()
+                }
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network Request Has Failed")
+            }
+        }
 
+    }
+
+    private suspend fun <T> myRetry(numberOfRetries: Int, block: suspend () -> T): T {
+        repeat(numberOfRetries) {
+            try {
+                return block()
+            } catch (exception: Exception) {
+                Timber.e(exception)
+            }
+        }
+        return block()
+    }
+
+    private suspend fun loadRequest() {
+        val recentVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentVersions)
     }
 
 }
