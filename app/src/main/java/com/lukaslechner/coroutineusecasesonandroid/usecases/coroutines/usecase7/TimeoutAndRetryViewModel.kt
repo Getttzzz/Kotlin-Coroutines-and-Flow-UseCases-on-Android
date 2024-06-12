1dc4d0b1f6b62fd616e95a22b2aa5a710e94ca92
Yurii Hetsko <getttzzz@gmail.com>
2024-06-13 00:44:18 +0200

    retry + timeout function

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
@@ -1,7 +1,14 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase7
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import timber.log.Timber
 
 class TimeoutAndRetryViewModel(
     private val api: MockApi = mockApi()
@@ -9,13 +16,67 @@ class TimeoutAndRetryViewModel(
 
     fun performNetworkRequest() {
         uiState.value = UiState.Loading
-        val numberOfRetries = 2
+
+        val retries = 2
         val timeout = 1000L
 
-        // TODO: Exercise 3
-        // switch to branch "coroutine_course_full" to see solution
+        val oreoVersionDeferred = viewModelScope.async {
+            retryWithTimeout(retries, timeout) {
+                api.getAndroidVersionFeatures(27)
+            }
+        }
 
-        // run api.getAndroidVersionFeatures(27) and api.getAndroidVersionFeatures(28) in parallel
+        val pieVersionDeferred = viewModelScope.async {
+            retryWithTimeout(retries, timeout) {
+                api.getAndroidVersionFeatures(28)
+            }
+        }
 
+        viewModelScope.launch {
+            try {
+                val versionFeatures = listOf(
+                    oreoVersionDeferred,
+                    pieVersionDeferred
+                )
+                    .awaitAll()
+
+                uiState.value = UiState.Success(versionFeatures)
+
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network Request Has Failed")
+
+            }
+        }
+    }
+
+    private suspend fun <T> retryWithTimeout(
+        retries: Int = 2,
+        timeout: Long,
+        block: suspend () -> T
+    ) = retryWithExponentialBackOff(retries) {
+        withTimeout(timeout) {
+            block()
+        }
+    }
+
+    private suspend fun <T> retryWithExponentialBackOff(
+        retries: Int = 2,
+        initialDelayMillis: Long = 100,
+        maxDelayMillis: Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T
+    ): T {
+        var currentDelay = initialDelayMillis
+        repeat(retries) {
+            try {
+                return block()
+            } catch (exception: Exception) {
+                Timber.e(exception)
+            }
+            delay(currentDelay)
+            currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayMillis)
+            println("GETZ.RetryNetworkRequestViewModel.retryWithExponentialBackOff--> currentDelay=$currentDelay")
+        }
+        return block()
     }
 }
